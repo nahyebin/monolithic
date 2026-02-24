4177b53124f8104bee107cad46f2c63d954b1d18
nahyebin <sju07305@naver.com>
2026-02-24 11:27:23 +0900

    feat: implements Order using JPA

--- src/main/java/com/example/monolithic/order/dao/OrderRepository.java
+++ src/main/java/com/example/monolithic/order/dao/OrderRepository.java
@@ -1,5 +1,10 @@
 package com.example.monolithic.order.dao;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.monolithic.order.domain.entity.OrderEntity;
+
+public interface OrderRepository extends JpaRepository<OrderEntity,Long> { 
+    //주문저장 프로세스 구현
     
 }
